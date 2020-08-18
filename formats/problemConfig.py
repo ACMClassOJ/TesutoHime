76877486e038ed054013e77919a572c7a6cc3ca9
XOR-op <17672363+XOR-op@users.noreply.github.com>
2020-08-18 12:40:07 +0800

    tmp

--- formats/problemConfig.py
+++ formats/problemConfig.py
@@ -13,8 +13,8 @@ class ProblemConfig:
         self.ID = ID
         self.language=language  # e.g. cmake,c++14,c++20,submitAnswer...
         self.SPJ=SPJ
-        self.timeLimit=timeLimit
-        self.memoryLimit=memoryLimit
-        self.compileTimeLimit=compileTimeLimit
-        self.diskLimit=diskLimit
+        self.timeLimit=timeLimit # ms
+        self.memoryLimit=memoryLimit # kb
+        self.compileTimeLimit=compileTimeLimit # ms
+        self.diskLimit=diskLimit # kb
         

bc954e85be72c2c850b723692eeeff87897ba94c
Pioooooo <bowen.sun@qq.com>
2020-08-20 08:17:51 +0800

    admin management

---
+++ Web/admin.py
@@ -0,0 +1,166 @@
+from flask import Flask, request, session
+from const import *
+from userManager import UserManager
+from problemManager import ProblemManager
+from contestManager import ConetstManager
+
+admin = Flask('ADMIN')
+
+user = UserManager()
+problem = ProblemManager()
+contest = ConetstManager()
+
+
+# TODO(Pioooooo): validate data
+def _validate_user_data(form):
+    if 'type' not in form:
+        return ReturnCode.ERR_BAD_DATA
+    # TODO: validate username
+    if 0 <= form['type'] < 2:
+        # TODO: validate
+        return None
+    return None
+
+
+def _validate_problem_data(form):
+    if 'type' not in form:
+        return ReturnCode.ERR_BAD_DATA
+    if 1 <= form['type'] < 3:
+        # TODO: validate ID
+        return None
+    if 0 <= form['type'] < 2:
+        # TODO: validate
+        return None
+    return None
+
+
+def _validate_contest_data(form):
+    if 'type' not in form:
+        return ReturnCode.ERR_BAD_DATA
+    if 1 <= form['type'] < 6:
+        # TODO: validate contest ID
+        return None
+    if 0 <= form['type'] < 2:
+        # TODO: validate
+        return None
+    elif 3 <= form['type'] < 5:
+        # TODO: validate problem ID
+        return None
+    elif 5 <= form['type'] < 6:
+        # TODO: validate username
+        return None
+    return None
+
+
+@admin.route('/')
+def index():
+    return 'index'
+
+
+@admin.route('/user', methods=['post'])
+def user_manager():
+    form = request.form
+    if String.SESSION not in session:
+        return ReturnCode.ERR_USER_NOT_LOGGED_IN
+    if user.Get_Privilege(session[String.SESSION]) < Privilege.SUPER:
+        return ReturnCode.ERR_INSUFFICIENT_PRIVILEGE
+    err = _validate_user_data(form)
+    if err is not None:
+        return err
+    if form[String.TYPE] == 0:
+        if user.Add_User(form[String.USERNAME], form[String.STUDENT_ID], form[String.FRIENDLY_NAME],
+                         form[String.PASSWORD],
+                         form[String.PRIVILEGE]):
+            return ReturnCode.SUC_ADD_USER
+        return ReturnCode.ERR_ADD_USER
+    elif form[String.TYPE] == 1:
+        if user.Modify_User(form[String.USERNAME], form[String.STUDENT_ID], form[String.FRIENDLY_NAME],
+                            form[String.PASSWORD],
+                            form[String.PRIVILEGE]):
+            return ReturnCode.SUC_MOD_USER
+        return ReturnCode.ERR_MOD_USER
+    elif form[String.TYPE] == 2:
+        if user.Delete_User(form[String.USERNAME]):
+            return ReturnCode.SUC_DEL_USER
+        return ReturnCode.ERR_DEL_USER
+    else:
+        return ReturnCode.ERR_BAD_DATA
+
+
+@admin.route('/problem', methods=['post'])
+def problem_manager():
+    form = request.form
+    if String.SESSION not in session:
+        return ReturnCode.ERR_USER_NOT_LOGGED_IN
+    if user.Get_Privilege(session[String.SESSION]) < Privilege.ADMIN:
+        return ReturnCode.ERR_INSUFFICIENT_PRIVILEGE
+    err = _validate_problem_data(form)
+    if err is not None:
+        return err
+    if form[String.TYPE] == 0:
+        if problem.Add_Problem(form[String.TITLE], form[String.DESCRIPTION], form[String.INPUT], form[String.OUTPUT],
+                               form[String.EXAMPLE_INPUT], form[String.EXAMPLE_OUTPUT], form[String.DATA_RANGE],
+                               form[String.RELEASE_TIME]):
+            return ReturnCode.SUC_ADD_PROBLEM
+        return ReturnCode.ERR_ADD_PROBLEM
+    elif form[String.TYPE] == 1:
+        if problem.Modify_Problem(form[String.PROBLEM_ID], form[String.TITLE], form[String.DESCRIPTION],
+                                  form[String.INPUT],
+                                  form[String.OUTPUT], form[String.EXAMPLE_INPUT], form[String.EXAMPLE_OUTPUT],
+                                  form[String.DATA_RANGE], form[String.RELEASE_TIME]):
+            return ReturnCode.SUC_MOD_PROBLEM
+        return ReturnCode.ERR_MOD_PROBLEM
+    elif form[String.TYPE] == 2:
+        if problem.Delete_Problem(form[String.PROBLEM_ID]):
+            return ReturnCode.SUC_DEL_PROBLEM
+        return ReturnCode.ERR_DEL_PROBLEM
+    else:
+        return ReturnCode.ERR_BAD_DATA
+
+
+@admin.route('/contest', methods=['post'])
+def contest_manager():
+    form = request.form
+    if String.SESSION not in session:
+        return ReturnCode.ERR_USER_NOT_LOGGED_IN
+    if user.Get_Privilege(session[String.SESSION]) < Privilege.ADMIN:
+        return ReturnCode.ERR_INSUFFICIENT_PRIVILEGE
+    err = _validate_contest_data(form)
+    if err is not None:
+        return err
+    if form[String.TYPE] == 0:
+        if contest.Create_Contest(form[String.CONTEST_NAME], form[String.START_TIME], form[String.END_TIME],
+                                  form[String.CONTEST_TYPE]):
+            return ReturnCode.SUC_ADD_CONTEST
+        return ReturnCode.ERR_ADD_CONTEST
+    elif form[String.TYPE] == 1:
+        if contest.Modify_Contest(form[String.CONTEST_ID], form[String.CONTEST_NAME], form[String.START_TIME],
+                                  form[String.END_TIME], form[String.CONTEST_TYPE]):
+            return ReturnCode.SUC_MOD_CONTEST
+        return ReturnCode.ERR_MOD_CONTEST
+    elif form[String.TYPE] == 2:
+        if contest.Delete_Contest(form[String.CONTEST_ID]):
+            return ReturnCode.SUC_DEL_CONTEST
+        return ReturnCode.ERR_DEL_CONTEST
+    elif form[String.TYPE] == 3:
+        if contest.Add_Problem_To_Contest(form[String.CONTEST_ID], form[String.CONTEST_PROBLEM_ID]):
+            return ReturnCode.SUC_ADD_PROBLEM_TO_CONTEST
+        return ReturnCode.ERR_ADD_PROBLEM_TO_CONTEST
+    elif form[String.TYPE] == 4:
+        if contest.Delete_Problem_From_Contest(form[String.CONTEST_ID], form[String.CONTEST_PROBLEM_ID]):
+            return ReturnCode.SUC_DEL_PROBLEM_FROM_CONTEST
+        return ReturnCode.ERR_DEL_PROBLEM_FROM_CONTEST
+    elif form[String.TYPE] == 5:
+        if contest.Add_Player_To_Contest(form[String.CONTEST_ID], form[String.CONTEST_USERNAME]):
+            return ReturnCode.SUC_DEL_CONTEST
+        return ReturnCode.ERR_DEL_CONTEST
+    elif form[String.TYPE] == 6:
+        if contest.Delete_Player_From_Contest(form[String.CONTEST_ID], form[String.CONTEST_USERNAME]):
+            return ReturnCode.SUC_DEL_CONTEST
+        return ReturnCode.ERR_DEL_CONTEST
+    else:
+        return ReturnCode.ERR_BAD_DATA
+
+
+if __name__ == '__main__':
+    admin.run()
---
+++ Web/const.py
@@ -0,0 +1,68 @@
+class String:
+    TYPE = 'type'
+    SESSION = 'username'
+    USERNAME = 'username'
+    STUDENT_ID = 'id'
+    FRIENDLY_NAME = 'name'
+    PASSWORD = 'password'
+    PRIVILEGE = 'privilege'
+    PROBLEM_ID = 'id'
+    TITLE = 'title'
+    DESCRIPTION = 'description'
+    INPUT = 'input'
+    OUTPUT = 'output'
+    EXAMPLE_INPUT = 'example_input'
+    EXAMPLE_OUTPUT = 'example_output'
+    DATA_RANGE = 'range'
+    RELEASE_TIME = 'time'
+    CONTEST_ID = 'contest_id'
+    CONTEST_NAME = 'name'
+    START_TIME = 'start_time'
+    END_TIME = 'end_time'
+    CONTEST_TYPE = 'contest_type'
+    CONTEST_PROBLEM_ID = 'id'
+    CONTEST_USERNAME = 'username'
+
+
+class ReturnCode:
+    SUC = {'e': 0}
+    SUC_ADD_USER = {'e': 0, 'msg': 'user added successfully'}
+    SUC_MOD_USER = {'e': 0, 'msg': 'user modified successfully'}
+    SUC_DEL_USER = {'e': 0, 'msg': 'user removed successfully'}
+    SUC_ADD_PROBLEM = {'e': 0, 'msg': 'problem added successfully'}
+    SUC_MOD_PROBLEM = {'e': 0, 'msg': 'problem modified successfully'}
+    SUC_DEL_PROBLEM = {'e': 0, 'msg': 'problem removed successfully'}
+    SUC_ADD_CONTEST = {'e': 0, 'msg': 'contest created successfully'}
+    SUC_MOD_CONTEST = {'e': 0, 'msg': 'contest modified successfully'}
+    SUC_DEL_CONTEST = {'e': 0, 'msg': 'contest removed successfully'}
+    SUC_ADD_PROBLEM_TO_CONTEST = {'e': 0, 'msg': 'problem added to contest successfully'}
+    SUC_DEL_PROBLEM_FROM_CONTEST = {'e': 0, 'msg': 'problem removed from contest successfully'}
+    SUC_ADD_USER_TO_CONTEST = {'e': 0, 'msg': 'user added to contest successfully'}
+    SUC_DEL_USER_FROM_CONTEST = {'e': 0, 'msg': 'user removed from contest successfully'}
+
+    ERR_BAD_DATA = {'e': -1, 'msg': 'bad data'}
+
+    ERR_USER_NOT_LOGGED_IN = {'e': -100, 'msg': 'user not logged in'}
+    ERR_INSUFFICIENT_PRIVILEGE = {'e': -101, 'msg': 'insufficient privilege'}
+
+    ERR_INVALID_USERNAME = {'e': -200, 'msg': 'invalid username'}
+
+    ERR_ADD_USER = {'e': -300, 'msg': 'failed to add user'}
+    ERR_MOD_USER = {'e': -301, 'msg': 'failed to modify user'}
+    ERR_DEL_USER = {'e': -302, 'msg': 'failed to remove user'}
+    ERR_ADD_PROBLEM = {'e': -303, 'msg': 'failed to add problem'}
+    ERR_MOD_PROBLEM = {'e': -304, 'msg': 'failed to modify problem'}
+    ERR_DEL_PROBLEM = {'e': -305, 'msg': 'failed to remove problem'}
+    ERR_ADD_CONTEST = {'e': -306, 'msg': 'failed to create contest'}
+    ERR_MOD_CONTEST = {'e': -307, 'msg': 'failed to modify contest'}
+    ERR_DEL_CONTEST = {'e': -308, 'msg': 'failed to remove contest'}
+    ERR_ADD_PROBLEM_TO_CONTEST = {'e': -309, 'msg': 'failed to add problem to contest'}
+    ERR_DEL_PROBLEM_FROM_CONTEST = {'e': -310, 'msg': 'failed to remove problem from contest'}
+    ERR_ADD_USER_TO_CONTEST = {'e': -311, 'msg': 'failed to add user to contest'}
+    ERR_DEL_USER_FROM_CONTEST = {'e': -312, 'msg': 'failed to remove user from contest'}
+
+
+class Privilege:
+    NORMAL = 0
+    ADMIN = 1
+    SUPER = 2

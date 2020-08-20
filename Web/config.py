5deda40a8dd7cdd687d4269fe37eff687431a95c
Amagi_Yukisaki <xingzeyu2001@gmail.com>
2020-08-20 09:47:48 +0800

    implemented sessionManager

--- Web/config.py
+++ Web/config.py
@@ -3,3 +3,6 @@ class DataBaseConfig:
     mysql_User = 'root'
     mysql_Password = 'Progynova'
     mysql_Database = 'OJ'
+
+class Login_Config:
+    Login_Life_Time = 24 * 60 * 60 # s
--- Web/utils.py
+++ Web/utils.py
@@ -1,5 +1,11 @@
 import pymysql
+import datetime
+import time
 from config import *
 
 def DB_Connect():
     return pymysql.connect(DataBaseConfig.mysql_Host, DataBaseConfig.mysql_User, DataBaseConfig.mysql_Password, DataBaseConfig.mysql_Database)
+
+def UnixNano() -> float: # float point time in Second
+    return time.time()
+

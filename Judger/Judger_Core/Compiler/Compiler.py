88acbe5ff7992b62638150e565c03f485007257f
Anoxiacxy <2819114191@qq.com>
2020-08-31 10:36:46 +0800

    compiler

---
+++ Judger/Judger_Core/Compiler/Compiler.py
@@ -0,0 +1,46 @@
+from Judger_Core.compiler_interface import CompilerInterface
+from Judger_Core.config import CompilationConfig, CompilationResult
+import subprocess
+
+
+
+class Compiler(CompilerInterface):
+    def compile_cpp(self, code, timeLimit):
+        codeFile = open("compiler/source_code.cpp", "w")
+        codeFile.write(code)
+        codeFile.close()
+        process = subprocess.run(
+            ["g++", "compiler/source_code.cpp"], 
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+            timeout = timeLimit / 1000)
+        if process.returncode != 0:
+            return CompilationResult(
+                compiled    = False,    
+                msg         = process.stderr,
+                programPath = None)
+        
+        process = subprocess.run(
+            ["g++", "compiler/source_code.cpp", "-E"], 
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+            timeout = timeLimit / 1000)
+        
+    def compile_git(self, url, timeLimit):
+
+        pass
+
+    def CompileInstance(self, code_config):
+        sourceCode  = code_config.sourceCode
+        language    = code_config.language
+        timeLimit   = code_config.compileTimeLimit
+        if language == "c++":
+            return compile_cpp(sourceCode, timeLimit)
+        elif language == "git":
+            return compile_git(sourceCode, timeLimit)
+        else:
+            return CompilationResult(
+                compiled    = False, 
+                msg         = "暂不支持用" + language + "语言提交",
+                programPath = None
+                )

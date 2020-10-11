4ce011791e6738c9ab1cb98bfcf0823714763e42
Amagi_Yukisaki <xingzeyu2001@gmail.com>
2020-10-11 14:15:11 +0800

    rebase files

---
+++ Web/static/js/user-avatar.js
@@ -0,0 +1,32 @@
+$(function(){
+	$.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "/get_username", 
+        data: {"type" : 1},
+        success: function (response_text)
+        {
+			if(response_text == "Nobody")
+			{
+				document.getElementById('navbar_left').innerHTML = '\
+					<ul class="navbar-nav navbar-nav-hover align-items-lg-right">\
+			        	<li class="nav-item d-none d-lg-block ml-lg-4">\
+				        	<a class="btn btn-neutral btn-icon">\
+	              				<span class="nav-link-inner--text">登录</span>\
+	            			</a>\
+	            		</li>\
+			          	<li class="nav-item d-none d-lg-block ml-lg-4">\
+				        	<a class="btn btn-neutral btn-icon">\
+	              				<span class="nav-link-inner--text">注册</span>\
+	            			</a>\
+	            		</li>\
+	    			</ul>;'
+			}
+			else
+			{
+				document.getElementById('navbar_left').innerHTML = '<div>欢迎您，' + response_text + '</div>';
+			}
+			
+        },
+    });
+});

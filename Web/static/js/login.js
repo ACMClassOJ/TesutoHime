cac4ded4dd58847e1f004d3193737a6f19d17ae1
cong258258 <1615223768@qq.com>
2020-10-11 16:37:56 +0800

    frontend register & login callback

--- Web/static/js/login.js
+++ Web/static/js/login.js
@@ -1,12 +1,25 @@
+var result = {};
+$(function(){
+    var url = window.location.href; 
+    url = url.substr(url.indexOf("?")+1);
+    var queryString = url || location.search.substring(1),
+        re = /([^&=]+)=([^&]*)/g,
+        m; 
+   	while (m = re.exec(queryString))
+        result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]); 
+});
+
 $(function(){
 	$("#login").ajaxForm(function(response_text)
 	{
 		if(response_text == 0)
 		{
-			swal("Success","登录成功，1秒后跳转主页","success");
-			setTimeout(function(){window.location.replace("../../index.html");},1000);
+			swal("Success","登录成功","success");
+			setTimeout(function(){
+				window.location.replace(result['next']);
+			},500);
 		}
-		else if(response_text == -1)
+		else
 			swal("Oops","用户名或密码错误","error");
 	});
 });
--- Web/static/js/register.js
+++ Web/static/js/register.js
@@ -1,10 +1,23 @@
+var result = {};
+$(function(){
+    var url = window.location.href; 
+    url = url.substr(url.indexOf("?")+1);
+    var queryString = url || location.search.substring(1),
+        re = /([^&=]+)=([^&]*)/g,
+        m; 
+   	while (m = re.exec(queryString))
+        result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]); 
+});
+
 $(function(){
 	$("#register").ajaxForm(function(response_text)
 	{
 		if(response_text == 0)
 		{
 			swal("Success","注册成功！您现在可以登陆了","success");
-			setTimeout(function(){window.location.replace("../../login.html");},1000);
+			setTimeout(function(){
+				window.location.replace("/");
+			},500);
 		}
 		else if(response_text == -1)
 			swal("Oops","注册失败，检查您的输入信息！","error");

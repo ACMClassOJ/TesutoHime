f0480789d3b67f82e29aa60e00842d20f25ccdfc
acrazyczy <a_crazy_czy@yahoo.com>
2020-10-13 09:17:17 +0800

    add performance_rate

--- Judger/config.py
+++ Judger/config.py
@@ -7,3 +7,4 @@ Web_Server_Secret = "justfortest"
 Web_Server = "http://localhost:5005"
 Heart_Beat_Period = 600000
 Judge_Result_Resend_Period = 1000
+Performance_Rate = 1.1 # real_run_time/oj_standard_time

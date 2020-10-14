207d6f1e26b8a6dfe6868ff63eb7b19a83f16181
Amagi_Yukisaki <xingzeyu2001@gmail.com>
2020-10-14 23:52:55 +0800

    added auto-setup system for judge-server

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir /exe/ && chmod 777 /exe/
-mkdir /work/ && chmod 777 /work/
-apt-get install  protobuf-compiler libnl-3-dev libnl-route-3-dev
-cp ./nsjail /bin
-chmod 0755 /bin/nsjail

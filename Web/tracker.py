1e022ea4e77d2bd3ecab9c7fa501a77a6551474d
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2020-10-23 16:45:19 +0800

    fixed bug in tracker_realname

--- Web/tracker.py
+++ Web/tracker.py
@@ -2,6 +2,7 @@ from flask import request
 import logging, json, logging.handlers
 from sessionManager import Login_Manager
 from referenceManager import Reference_Manager
+from userManager import User_Manager
 from config import LogConfig
 from utils import *
 
@@ -14,7 +15,7 @@ class Tracker:
         everything = {}
         everything['IP'] = request.remote_addr
         everything['Username'] = Login_Manager.get_username()
-        everything['Realname'] = Reference_Manager.Query_Realname(everything['Username'])
+        everything['Realname'] = Reference_Manager.Query_Realname(str(User_Manager.Get_Student_ID(str(everything['Username']))))
         everything['url'] = request.url
         everything['post_args'] = request.form
         if not drop_args:

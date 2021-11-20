a3a246c29a23fca3d88dacd1b04b2ea98637a20d
stneng <git@stneng.com>
2021-11-20 18:40:43 +0800

    Revert "feat: migrate session to redis for multi-threading"
    This reverts commit a669f594d703a3e722205908e97cf9c8f99b7d6e.

--- Web/sessionManager.py
+++ Web/sessionManager.py
@@ -1,44 +1,35 @@
 from userManager import UserManager
 from flask import request
-import redis
-import config
 
 
 class SessionManager:
     def __init__(self):
-        self.redis = redis.StrictRedis(host=config.RedisConfig.host, port=config.RedisConfig.port,
-                                       db=config.RedisConfig.db)
+        self.mem = {}
         return
-
-    def get(self, lid: str) -> str:
-        ret = self.redis.get(str(lid))
-        if ret == None:
-            return ''
-        return ret.decode('UTF-8')
 
     def check_user_status(self) -> bool:  # to check whether current user has logged in properly
         lid = request.cookies.get('Login_ID')
-        return bool(self.get(lid))
+        return lid in self.mem
 
     def new_session(self, username: str, login_id: str):
-        self.redis.set(login_id, username, ex=60 * 60 * 24 * 30)  # expire after 30 days
+        self.mem[login_id] = username
         return
 
     def get_username(self) -> str:
         lid = request.cookies.get('Login_ID')
-        return self.get(lid)
+        return self.mem[lid] if lid in self.mem else ''
 
     def get_friendly_name(self) -> str:
         lid = request.cookies.get('Login_ID')
-        if not (self.get(lid)):
+        if not (lid in self.mem):
             return ''
-        return UserManager().get_friendly_name(self.get(lid))
+        return UserManager().get_friendly_name(self.mem[lid])
 
     def get_privilege(self) -> int:
         lid = request.cookies.get('Login_ID')
-        if not (self.get(lid)):
+        if not (lid in self.mem):
             return -1  # lowest Privilege for Guests
-        return UserManager().get_privilege(self.get(lid))
+        return UserManager().get_privilege(self.mem[lid])
 
 
 Login_Manager = SessionManager()

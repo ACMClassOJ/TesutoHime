0a7bd24615772b4e3a79f7ffa381216b1753b493
cong258258 <1615223768@qq.com>
2022-03-09 19:47:43 +0800

    [Frontend] avoid duplicate join in contest/homework

--- Web/static/js/post_join.js
+++ Web/static/js/post_join.js
@@ -5,6 +5,9 @@ function post_join(x)  {
             dataType: "text",
             data: {contest_id: x},
             url: "/OnlineJudge/api/join",
+            beforeSend: function () {
+                $("#join_button_" + x).attr("disabled", "disabled");
+            },
             success: function (response_text)  {
                     location.reload();
             },

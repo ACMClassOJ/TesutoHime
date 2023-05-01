e71befe07d6af3f919392b3b0f54b9c0d88bf332
Zonghan YANG <fstqwq@foxmail.com>
2023-05-01 11:00:43 +0000

    fix(web/admin): prevent double commit in the judge tab

--- Web/static/js/admin.js
+++ Web/static/js/admin.js
@@ -211,12 +211,21 @@ $(function () {
 
 
     var ajaxForms = [ '#formRejudge', '#formRejudge2', '#formDisableJudge', '#formDisableJudge2', '#formRealname', '#form-abort', '#form-abort-2' ]
+    var ajaxButtons = [ '#btnRejudge', '#btnRejudge2', '#btnDisableJudge', '#btnDisableJudge2', '#btnAddRealname', '#btnAbortJudge', '#btnAbortJudge2' ]
+    // disable when submit
+    for (btn of ajaxButtons) {
+        $(btn).attr('onclick', 'for (btn of ajaxButtons) {$(btn).attr("disabled", true);}');
+    }
     for (form of ajaxForms) {
         $(form).ajaxForm(function (ret_json) {
             if(ret_json['e'] < 0)
                 swal("Error " + ret_json['e'], ret_json['msg'], "error");
             else
                 swal("Success", ret_json['msg'], "success");
+            // enable when receive
+            for (btn of ajaxButtons) {
+                $(btn).removeAttr('disabled');
+            }
         });
     }
 });

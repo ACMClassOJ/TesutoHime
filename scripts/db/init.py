f86b4dbfc5e6a5c4bf3e1208b11ec0c9835444fc
Alan Liang <symb@olic.link>
2023-10-03 12:00:52 +0800

    fix(scripts): init alembic version on init

--- scripts/db/init.py
+++ scripts/db/init.py
@@ -1,9 +1,13 @@
+from alembic import command
+from alembic.config import Config
 from commons.models import metadata
 from scripts.db.env import engine
 
 
 def init_db():
     metadata.create_all(engine)
+    alembic_cfg = Config('alembic.ini')
+    command.stamp(alembic_cfg, 'head')
 
 if __name__ == '__main__':
     init_db()

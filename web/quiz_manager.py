59667df3454fbad9bcde2f402aee513d0214b295
Alan Liang <symb@olic.link>
2024-01-24 15:49:32 +0800

    fix(web): quiz handling

--- web/quiz_manager.py
+++ web/quiz_manager.py
@@ -14,7 +14,7 @@ class QuizManager:
         zip_path = os.path.join(config.cache_dir, key)
         s3_internal.download_file(S3Config.Buckets.problems, key, zip_path)
         with zipfile.ZipFile(zip_path, 'r') as zip:
-            with zip.open('quiz.json') as f:
+            with zip.open(f'{id}/quiz.json') as f:
                 try:
                     main_json = json.loads(f.read())
                 except json.JSONDecodeError:

b14cc4e9e085694cd9dc6ccf3e94266da55c37c9
Alan Liang <symb@olic.link>
2024-02-17 21:55:24 +0800

    fix(scripts): export

--- scripts/db/export.py
+++ scripts/db/export.py
@@ -4,6 +4,7 @@ from typing import *
 
 from boto3 import client as s3_client
 from botocore.config import Config
+from sqlalchemy import select
 
 from commons.task_typing import *
 from commons.util import deserialize
@@ -16,7 +17,7 @@ s3 = s3_client('s3', **s3_connection, config=cfg)
 db = Session()
 
 def filename (s: JudgeRecordV2) -> str:
-    return f'{s.user.student_id}-{names[s.user.user_id]}-{s.user.username}-{s.id}-T{s.problem.id}-{s.status.name}-{s.score}.cpp'
+    return f'{s.user.student_id}-{names[s.user.id]}-{s.user.username}-{s.id}-T{s.problem.id}-{s.status.name}-{s.score}.cpp'
 
 def prelude (s: JudgeRecordV2) -> str:
     details_message = 'No details'
@@ -28,7 +29,7 @@ def prelude (s: JudgeRecordV2) -> str:
         details_message = '\n * '.join(f'{group.name}: {group.score} {group.result}' for group in details.groups)
     return f'''
 /**
- * {s.user.student_id} {names[s.user.user_id]} ({s.user.username})
+ * {s.user.student_id} {names[s.user.id]} ({s.user.username})
  * Problem {s.problem_id} - {s.problem.title}
  * Time: {s.created_at.strftime('%Y-%m-%d %H:%M:%S')}
  * Status: {s.status.name}
@@ -51,13 +52,25 @@ def get_realname(user):
     if len(rec) == 0: return 'Unknown'
     return rec[-1][0]
 
-contest = db.query(Contest).where(Contest.id == contest_id).one()
-names = dict((x.user_id, get_realname(x)) for x in contest.players)
+contest = db.get(Contest, contest_id)
+
+def get_implicit_players(contest: Contest) -> Sequence[User]:
+    stmt = select(RealnameReference) \
+        .where(RealnameReference.course_id == contest.course_id)
+    if contest.group_ids is not None:
+        stmt = stmt.where(select(GroupRealnameReference)
+                          .where(GroupRealnameReference.realname_reference_id == RealnameReference.id)
+                          .where(GroupRealnameReference.group_id.in_(contest.group_ids))
+                          .exists())
+    return [e.user for rr in db.scalars(stmt) for e in rr.enrollments if not e.admin]
+
+players = get_implicit_players(contest)
+names = dict((x.id, get_realname(x)) for x in players)
 problems = db.query(ContestProblem.problem_id).where(ContestProblem.contest_id == contest_id).all()
 submissions: List[JudgeRecordV2] = db \
     .query(JudgeRecordV2) \
     .where(JudgeRecordV2.problem_id.in_(x[0] for x in problems)) \
-    .where(JudgeRecordV2.user_id.in_(x.user_id for x in contest.players)) \
+    .where(JudgeRecordV2.user_id.in_(x.id for x in players)) \
     .where(JudgeRecordV2.created_at >= contest.start_time) \
     .where(JudgeRecordV2.created_at < contest.end_time) \
     .all()

b16be38474d08b4513a3aec2f086b92c5caf951d
Liang Yalun <symb@olic.link>
2026-01-18 22:17:25 +0800

    feat(judger2/stdenv): fix random seed of gcc for reproducible output

--- judger2/sandbox/stdenv/flake.nix
+++ judger2/sandbox/stdenv/flake.nix
@@ -11,24 +11,27 @@
     inherit(nixpkgs) lib;
 
     profiles = import ./profiles.nix pkgs;
+
+    fixRandomSeed = {
+      # This is actually incorrect use of -frandom-seed.
+      # See https://github.com/NixOS/nixpkgs/issues/153793
+      NIX_OUTPATH_USED_AS_RANDOM_SEED = "TesutoHime";
+    };
+
     buildEnv = suffix: extraPkgs: lib.mapAttrs' (name: value: {
       name = "${name}${suffix}";
-      value = pkgs.buildEnv {
-        # for `nix bundle`
-        pname = "acmoj-stdenv-${name}${suffix}";
-        meta.mainProgram = "bash";
-
+      value = (pkgs.buildEnv {
         name = "acmoj-stdenv-${name}${suffix}";
         paths = value ++ extraPkgs;
         postBuild = "ln -s . $out/usr";
-      };
+      }).overrideAttrs fixRandomSeed;
     }) profiles;
     mkDevelop = suffix: extraPkgs: lib.mapAttrs' (name: value: {
       name = "${name}${suffix}";
-      value = pkgs.stdenv.mkDerivation {
+      value = (pkgs.stdenv.mkDerivation {
         name = "${name}${suffix}";
         buildInputs = value;
-      };
+      }).overrideAttrs fixRandomSeed;
     }) profiles;
   in {
     packages."${system}" = (buildEnv "" []) // (buildEnv "-shell" [ pkgs.bashInteractive ]);
